9d45b41d37534d75829b4a5e60142804a2d26e1c
59exe <72470367+59exe@users.noreply.github.com>
2021-09-03 21:16:47 +0100

    initial upload of files to repo

---
+++ Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Snow
+        Me.Button1.Location = New System.Drawing.Point(3053, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button2.Font = New System.Drawing.Font("Magic Mushroom", 71.99999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(1068, 350)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(963, 843)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "0"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(59, 177)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(117, 99)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Buy Double Click" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(100pts)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(59, 273)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(117, 99)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Buy Triple Click" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(500pts)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(59, 362)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(117, 99)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Buy Quad Click" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(1100pts)"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(59, 454)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(117, 99)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Buy Five Clicks" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(3000pts)"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(2922, 1278)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(117, 99)
+        Me.Button0.TabIndex = 6
+        Me.Button0.Text = "hehe click this to crash"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(59, 544)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(117, 99)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "Buy Six Clicks" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(8000pts)"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(59, 634)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(117, 99)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "Buy Seven Clicks" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(11000pts)"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(1757, 350)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(274, 191)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Random_Clicker_Game.My.Resources.Resources.randomclicker
+        Me.ClientSize = New System.Drawing.Size(3114, 1433)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "rcg"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+
+    Dim TwoClicks As Boolean = False
+    Dim ThreeClicks As Boolean = False
+    Dim QuadClicks As Boolean = False
+    Dim QuintClicks As Boolean = False
+    Dim SixClicks As Boolean = False
+    Dim SevenClicks
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Environment.Exit(420)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (TwoClicks = True) Then
+            Button2.Text = Button2.Text + 2
+        ElseIf ThreeClicks = True Then
+            Button2.Text = Button2.Text + 3
+        ElseIf QuadClicks = True Then
+            Button2.Text = Button2.Text + 4
+        ElseIf QuintClicks = True Then
+            Button2.Text = Button2.Text + 5
+        ElseIf SixClicks = True Then
+            Button2.Text = Button2.Text + 6
+        Else
+            Button2.Text = Button2.Text + 1
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (Button2.Text >= 100) Then
+            If (TwoClicks = False) Then
+                Button2.Text = Button2.Text - 100
+                TwoClicks = True
+                Button3.Text = "Buy Double Click (Bought)"
+            Else
+                MsgBox("You have already unlocked this power-up!")
+            End If
+        Else
+            If TwoClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 100 Then
+                MsgBox("You cannot afford this!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (Button2.Text >= 500) Then
+            If (ThreeClicks = False) Then
+                Button2.Text = Button2.Text - 500
+                ThreeClicks = True
+                Button4.Text = "Buy Triple Click (Bought)"
+                TwoClicks = False
+            Else
+                MsgBox("You have already unlocked this power-up!")
+            End If
+        Else
+            If ThreeClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 500 Then
+                MsgBox("You cannot afford this!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (Button2.Text >= 1100) Then
+            If (QuadClicks = False) Then
+                Button2.Text = Button2.Text - 1100
+                QuadClicks = True
+                Button5.Text = "Buy Quad Click (Bought)"
+                ThreeClicks = False
+            Else
+                MsgBox("You have already unlocked this power-up!")
+            End If
+        Else
+            If QuadClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 1100 Then
+                MsgBox("You cannot afford this!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If Button2.Text >= 3000 Then
+            If QuintClicks = False Then
+                Button2.Text = Button2.Text - 3000
+                QuintClicks = True
+                Button6.Text = "Buy Five Clicks (Bought)"
+                QuadClicks = False
+            Else
+                MsgBox("You have already unlocked this power-up!")
+            End If
+        Else
+            If QuintClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 3000 Then
+                MsgBox("You cannot afford this!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        MsgBox("Error: Program will now be terminated.")
+        Throw New NotImplementedException()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If Button2.Text >= 8000 Then
+            If SixClicks = False Then
+                Button2.Text = Button2.Text - 8000
+                SixClicks = True
+                Button7.Text = "Buy Six Clicks (Bought)"
+                QuintClicks = False
+            Else
+                MsgBox("You already unlocked this power-up!")
+            End If
+        Else
+            If SixClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 8000 Then
+                MsgBox("You cannot afford this!")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If Button2.Text >= 11000 Then
+            If SevenClicks = False Then
+                Button2.Text = Button2.Text - 11000
+                SevenClicks = True
+                Button8.Text = "Buy Seven Clicks (Bought)"
+                SixClicks = False
+            Else
+                MsgBox("You already unlocked this power-up!")
+            End If
+        Else
+            If SevenClicks = True Then
+                MsgBox("You have already unlocked this power-up!")
+            ElseIf Button2.Text < 11000 Then
+                MsgBox("ur broke my g")
+            End If
+        End If
+    End Sub
+End Class
